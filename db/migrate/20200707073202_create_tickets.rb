bf3840ba5bd6c192d5d2675cfb168c666d229abc
willnet <netwillnet@gmail.com>
2020-07-07 16:33:59 +0900

    Ticketモデルのマイグレーションファイルを修正
    不要なnot null 制約を削除
    不要な外部キー制約を削除
    複合ユニークインデックスの追加
    不要なインデックス削除

--- db/migrate/20200707073202_create_tickets.rb
+++ db/migrate/20200707073202_create_tickets.rb
@@ -1,11 +1,12 @@
 class CreateTickets < ActiveRecord::Migration[6.0]
   def change
     create_table :tickets do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :event, null: false, foreign_key: true
+      t.references :user
+      t.references :event, null: false, foreign_key: true, index: false
       t.string :comment
-
       t.timestamps
     end
+
+    add_index :tickets, %i[event_id user_id], unique: true
   end
 end
