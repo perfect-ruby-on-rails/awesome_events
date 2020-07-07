49b17029ed865f35e932fc540ca707196d6ced0f
willnet <netwillnet@gmail.com>
2020-07-07 17:16:59 +0900

    削除時に実行されるコールバックを追加、および関連を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  before_destroy :check_all_events_finished
+
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
@@ -12,5 +15,20 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントが存在します。"
+    end
+
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します。"
+    end
+
+    throw(:abort) unless errors.empty?
   end
 end
