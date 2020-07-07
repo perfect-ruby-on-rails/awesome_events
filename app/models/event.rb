e43650e00cbfff8ddb17c89091ef687ec6f69f53
willnet <netwillnet@gmail.com>
2020-07-07 17:11:12 +0900

    イベント削除時に関連レコードも削除するようにした

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: "User"
 
   validates :name, length: { maximum: 50 }, presence: true
