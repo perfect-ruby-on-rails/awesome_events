cc2de90e6c3dce5550a18ef59aae1df09266eaa0
willnet <netwillnet@gmail.com>
2020-07-07 15:55:49 +0900

    ログイン不要でアクセス可能なアクションを設定した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate, only: :create
+
   def create
     user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
     session[:user_id] = user.id
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate
+
   def index
   end
 end
