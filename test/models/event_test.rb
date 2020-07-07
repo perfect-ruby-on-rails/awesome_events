1e86cd7c8863f6bc3d4893b03b2de1da357f6ccc
willnet <netwillnet@gmail.com>
2020-07-07 18:17:21 +0900

    Event#created_by?のテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#created_by? owner_id と 引数の#id が同じとき' do
+    event = FactoryBot.create(:event)
+    assert_equal(true, event.created_by?(event.owner))
+  end
+
+  test '#created_by? owner_id と 引数の#id が異なるとき' do
+    event = FactoryBot.create(:event)
+    another_user = FactoryBot.create(:user)
+    assert_equal(false, event.created_by?(another_user))
+  end
+
+  test '#created_by? 引数が nil なとき' do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.created_by?(nil))
+  end
 end
