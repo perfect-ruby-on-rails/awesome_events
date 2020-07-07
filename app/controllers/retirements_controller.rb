463f9c017eb6a55f63e6114fe33fd8379cf85393
willnet <netwillnet@gmail.com>
2020-07-07 17:14:21 +0900

    退会用のアクションを追加

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -1,2 +1,11 @@
 class RetirementsController < ApplicationController
+  def new
+  end
+
+  def create
+    if current_user.destroy
+      reset_session
+      redirect_to root_path, notice: "退会完了しました"
+    end
+  end
 end
