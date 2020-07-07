cd8451ba70acb4d58dc07b8fd2938fd6738018ac
willnet <netwillnet@gmail.com>
2020-07-07 16:34:45 +0900

    Ticketモデルへのバリデーションを追加

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :event
+
+  validates :comment, length: { maximum: 30 }, allow_blank: true
 end
