19a369c7eb0b6bc15ade0816f6a4f4a3b2814d8c
willnet <netwillnet@gmail.com>
2020-07-07 15:54:46 +0900

    認証用のメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
+  before_action :authenticate
+  helper_method :logged_in?, :current_user
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
