efe78305a33def875fef76d0bba90d871c4c7cd5
willnet <netwillnet@gmail.com>
2020-07-07 17:47:05 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_064259) do
+ActiveRecord::Schema.define(version: 2020_07_07_073202) do
 
   create_table "events", force: :cascade do |t|
     t.bigint "owner_id"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2020_07_07_064259) do
     t.index ["owner_id"], name: "index_events_on_owner_id"
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id", null: false
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id", "user_id"], name: "index_tickets_on_event_id_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false
     t.string "uid", null: false
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_07_07_064259) do
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
+  add_foreign_key "tickets", "events"
 end
