743ce2ab44c91197cf8a38f28dcf3b432576a3fa
willnet <netwillnet@gmail.com>
2020-07-07 18:11:37 +0900

    機能テストでもログイン用のヘルパーメソッドを利用可能にした

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -7,12 +7,23 @@ module SignInHelper
       info: { nickname: user.name,
               image: user.image_url })
 
-    visit root_url
-    click_on "GitHubでログイン"
+    case
+    when respond_to?(:visit)
+      visit root_url
+      click_on "GitHubでログイン"
+    when respond_to?(:get)
+      get "/auth/github/callback"
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
 
   def current_user
     @current_user
   end
+end
+
+class ActionDispatch::IntegrationTest #((2))
+  include SignInHelper #((3))
 end
