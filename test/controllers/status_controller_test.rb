ac83d11055cd7d97f3ac2b478f5096841ff7ebdc
willnet <netwillnet@gmail.com>
2020-07-07 18:08:20 +0900

    起動確認APIのテストを追加

--- test/controllers/status_controller_test.rb
+++ test/controllers/status_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StatusControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET /status" do
+    get "/status"
+    assert_response(:success)
+    assert_equal({ status: "ok" }.to_json, @response.body)
+    assert_equal("application/json", @response.media_type)
+  end
 end
