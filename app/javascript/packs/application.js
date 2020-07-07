717bcfc2c21e5b7c3a6b0891acd4aad07d6aea90
willnet <netwillnet@gmail.com>
2020-07-07 15:08:13 +0900

    Bootstrapを利用できるようにした

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 
+import "bootstrap"
+import "bootstrap/scss/bootstrap.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
