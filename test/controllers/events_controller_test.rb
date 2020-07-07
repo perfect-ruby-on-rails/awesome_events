46c0d051edade2b321a573a6cebb5a0eb99b5bba
willnet <netwillnet@gmail.com>
2020-07-07 18:14:26 +0900

    他の人が作ったイベントは削除できないことを確認するテストを追加

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -9,4 +9,16 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
       delete event_url(event)
     end
   end
+
+  test "他の人がつくったイベントは削除できない" do
+    event_owner = FactoryBot.create(:user)
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_user = FactoryBot.create(:user)
+    sign_in_as sign_in_user
+    assert_difference("Event.count", 0) do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        delete event_url(event)
+      end
+    end
+  end
 end
