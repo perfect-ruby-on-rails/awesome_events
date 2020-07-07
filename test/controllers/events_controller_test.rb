f7be21adc1b44e5f491567399911c5c5f1c81c9c
willnet <netwillnet@gmail.com>
2020-07-07 18:13:25 +0900

    自分が作ったイベントを削除するテストを追加

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "自分がつくったイベントは削除できる" do
+    event_owner = FactoryBot.create(:user)
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_as event_owner
+    assert_difference("Event.count", -1) do
+      delete event_url(event)
+    end
+  end
 end
