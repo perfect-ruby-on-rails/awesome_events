bd00d5d841a404de8cea4ea74f0df5d1b1f8092a
willnet <netwillnet@gmail.com>
2020-07-07 15:17:03 +0900

    bin/rails g model user provider uid name image_url

---
+++ db/migrate/20200707061657_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
