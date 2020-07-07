d07f84d4b684b017b3db527decc90633405e96bf
willnet <netwillnet@gmail.com>
2020-07-07 15:37:51 +0900

    SessionsController#destroyを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
   end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
+  end
 end
