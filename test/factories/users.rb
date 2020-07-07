1c8c2625bc8d5b06638e28a30da4ee8ec8d1246c
willnet <netwillnet@gmail.com>
2020-07-07 17:25:29 +0900

    User用のFactory Botの定義を追加

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, aliases: [:owner] do
+    provider { "github" }
+    sequence(:uid) { |i| "uid#{i}" }
+    sequence(:name) { |i| "name#{i}" }
+    sequence(:image_url) { |i| "http://example.com/image#{i}.jpg"}
   end
 end
