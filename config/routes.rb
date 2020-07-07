73ac21cffd6636b3505255b14bc0f2a9acb6575e
willnet <netwillnet@gmail.com>
2020-07-07 15:05:25 +0900

    トップページのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "welcome#index"
 end
