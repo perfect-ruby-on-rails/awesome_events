04d1b2f6cd977edd3a355e3e9b61bbef59769f97
willnet <netwillnet@gmail.com>
2020-07-07 17:08:39 +0900

    イベント参加者を表示するようにした

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
   def new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def url_for_github(user)
+    "https://github.com/#{user.name}"
+  end
 end
