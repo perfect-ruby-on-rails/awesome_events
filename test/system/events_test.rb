3a83026e598c6824d784bfdb45eaddfa50817c60
willnet <netwillnet@gmail.com>
2020-07-07 18:04:43 +0900

    イベントを削除するテストを追加

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -44,4 +44,16 @@ class EventsTest < ApplicationSystemTestCase
     click_on "登録する"
     assert_selector "div.alert", text: "作成しました"
   end
+
+  test "/events/:id ページを表示して削除ボタンを押す" do
+    sign_in_as(FactoryBot.create(:user))
+    event = FactoryBot.create(:event, owner: current_user)
+    visit event_url(event)
+    assert_difference("Event.count", -1) do
+      accept_confirm do
+        click_on "イベントを削除する"
+      end
+      assert_selector "div.alert", text: "削除しました"
+    end
+  end
 end
