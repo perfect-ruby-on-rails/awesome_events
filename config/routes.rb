a38090ce31b41b6a7c94e12a539840ead68743ad
willnet <netwillnet@gmail.com>
2020-07-07 16:37:07 +0900

    イベント参加用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :events do
+    resources :tickets
+  end
 end
