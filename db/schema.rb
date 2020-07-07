aa3507fbead6de134eae59ffe2daf5a9f4c3a77f
willnet <netwillnet@gmail.com>
2020-07-07 15:52:08 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_061657) do
+ActiveRecord::Schema.define(version: 2020_07_07_064259) do
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "owner_id"
+    t.string "name", null: false
+    t.string "place", null: false
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_events_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false
