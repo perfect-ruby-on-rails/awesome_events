a4072baa14c47f22db733097dbd6205cd62f7bc8
willnet <netwillnet@gmail.com>
2020-07-07 17:13:17 +0900

    退会の確認ページに遷移できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
 
+  resource :retirements
+
   resources :events do
     resources :tickets
   end
