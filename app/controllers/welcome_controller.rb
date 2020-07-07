1536681ca51df9ef7a60ad1f85a2e701ce285aa4
willnet <netwillnet@gmail.com>
2020-07-07 16:24:09 +0900

    トップページでイベント一覧を表示できるようにした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
