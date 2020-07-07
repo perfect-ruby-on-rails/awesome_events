e14accc687aea178f0f01ffee08301f518343163
willnet <netwillnet@gmail.com>
2020-07-07 15:18:33 +0900

    Userモデルのマイグレーションファイルを修正
    not null制約を追加
    ユニークインデックスを追加

--- db/migrate/20200707061657_create_users.rb
+++ db/migrate/20200707061657_create_users.rb
@@ -1,12 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
-      t.string :name
-      t.string :image_url
-
+      t.string :provider,  null: false
+      t.string :uid,       null: false
+      t.string :name,      null: false
+      t.string :image_url, null: false
       t.timestamps
     end
+
+    add_index :users, %i[provider uid], unique: true
   end
 end
