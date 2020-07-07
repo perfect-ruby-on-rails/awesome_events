24927aa6b8b1be226a0b7e377e90670b03b22f5c
willnet <netwillnet@gmail.com>
2020-07-07 18:22:45 +0900

    Event#start_at_should_be_before_end_atのテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -18,4 +18,22 @@ class EventTest < ActiveSupport::TestCase
     event = FactoryBot.create(:event)
     assert_equal(false, event.created_by?(nil))
   end
+
+  test 'start_at_should_be_before_end_at validation OK' do
+    start_at = rand(1..30).days.from_now
+    end_at = start_at + rand(1..30).hours
+    event = FactoryBot.build(:event,
+                             start_at: start_at, end_at: end_at)
+    event.valid?
+    assert_empty(event.errors[:start_at])
+  end
+
+  test 'start_at_should_be_before_end_at validation error' do
+    start_at = rand(1..30).days.from_now
+    end_at = start_at - rand(1..30).hours
+    event = FactoryBot.build(:event,
+                             start_at: start_at, end_at: end_at)
+    event.valid?
+    assert_not_empty(event.errors[:start_at])
+  end
 end
