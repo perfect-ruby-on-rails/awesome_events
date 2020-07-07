3260e2f2cf28a349f40152eae039aa11703d6c4f
willnet <netwillnet@gmail.com>
2020-07-07 15:35:22 +0900

    User.find_or_create_from_auth_hash! を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
+
+    User.find_or_create_by!(provider: provider, uid: uid) do |user| # ((1))
+      user.name = nickname
+      user.image_url = image_url
+    end
+  end
 end
