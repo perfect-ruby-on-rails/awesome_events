77bd17c368674b4e605c48c6c4b4f5d1c0c88816
willnet <netwillnet@gmail.com>
2020-07-07 17:46:12 +0900

    トップページが正しく表示されているか確認するテストを追加

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class WelcomesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit welcomes_url
-  #
-  #   assert_selector "h1", text: "Welcome"
-  # end
+  test "/ ページを表示" do
+    visit root_url
+
+    assert_selector "h1", text: "イベント一覧"
+  end
 end
