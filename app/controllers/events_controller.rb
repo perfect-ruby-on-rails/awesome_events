0f210638c6c095247e755d7e5ade10af7999d027
willnet <netwillnet@gmail.com>
2020-07-07 15:57:02 +0900

    イベント登録用のアクションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "作成しました"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_at, :end_at
+    )
+  end
 end
