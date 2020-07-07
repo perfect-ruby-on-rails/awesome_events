34be1a0cdb5750e8201e5518a317e51278c203bb
willnet <netwillnet@gmail.com>
2020-07-07 15:52:39 +0900

    Eventモデルにバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ApplicationRecord
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :place, length: { maximum: 100 }, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :start_at_should_be_before_end_at
+
+  private
+
+  def start_at_should_be_before_end_at
+    return unless start_at && end_at
+
+    if start_at >= end_at
+      errors.add(:start_at, "は終了時間よりも前に設定してください")
+    end
+  end
 end
