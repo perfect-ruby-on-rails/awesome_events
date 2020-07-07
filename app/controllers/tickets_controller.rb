eccadb785086e057bb8a1946e097d32a7fdd149a
willnet <netwillnet@gmail.com>
2020-07-07 16:38:09 +0900

    イベント参加用のアクションを追加

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,16 @@
 class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingError, "ログイン状態で TicketsController#new にアクセス"
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+    if @ticket.save
+      redirect_to event, notice: "このイベントに参加表明しました"
+    end
+  end
 end
