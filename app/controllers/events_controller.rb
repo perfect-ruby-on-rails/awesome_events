5e26e258644a46c9a6bc03ae696555d6c5032b07
willnet <netwillnet@gmail.com>
2020-07-07 16:26:56 +0900

    イベント編集用のアクションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,17 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました"
+    end
+  end
+
   private
 
   def event_params
