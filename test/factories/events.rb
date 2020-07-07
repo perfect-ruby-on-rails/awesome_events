24ac83ea6767b38b89cb4b53526e035dddaeb857
willnet <netwillnet@gmail.com>
2020-07-07 17:26:31 +0900

    Event用のFactory Botの定義を追加

--- test/factories/events.rb
+++ test/factories/events.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :event do
-    
+    owner
+    sequence(:name) { |i| "イベント名#{i}" }
+    sequence(:place) { |i| "イベント開催場所#{i}" }
+    sequence(:content) { |i| "イベント本文#{i}" }
+    start_at { rand(1..30).days.from_now }
+    end_at { start_at + rand(1..30).hours }
   end
 end
