b377f6fbf76a665d093f89d2e05b4b70563747f2
willnet <netwillnet@gmail.com>
2020-07-07 18:06:27 +0900

    イベント一覧の表示条件を確認するテストを追加

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -6,4 +6,15 @@ class WelcomesTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "イベント一覧"
   end
+
+  test "/ ページを表示して、未来のイベントは表示、過去のイベントは非表示であること" do
+    future_event = FactoryBot.create(:event, start_at: Time.zone.now + 3.day)
+    past_event = FactoryBot.create(:event, start_at: Time.zone.now + 1.day)
+
+    travel_to Time.zone.now + 2.day do
+      visit root_url
+      assert_selector "h5", text: future_event.name
+      assert_no_selector "h5", text: past_event.name
+    end
+  end
 end
