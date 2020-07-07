935b16f2d9ae3727ca57cdcba5784b201cfbc12e
willnet <netwillnet@gmail.com>
2020-07-07 18:20:48 +0900

    モックを使うように修正

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,10 +3,9 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   test '#created_by? owner_id と 引数の#id が同じとき' do
     event = FactoryBot.create(:event)
-    user = User.new
-    user.stub(:id, event.owner_id) do
-      assert_equal(true, event.created_by?(user))
-    end
+    user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    assert_equal(true, event.created_by?(user))
+    user.verify
   end
 
   test '#created_by? owner_id と 引数の#id が異なるとき' do
