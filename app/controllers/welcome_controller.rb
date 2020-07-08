3034fd982d0a2f813b0d6fbbe3a71733e378d9d1
willnet <netwillnet@gmail.com>
2020-07-08 09:38:50 +0900

    イベント一覧を10件ごとにページネーションするようにした

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
-    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
+    @events = Event.page(params[:page]).per(10).
+      where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
