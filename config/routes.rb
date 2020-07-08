1e82da7d1977493ec9f4445b62a38d40a53eb0e0
willnet <netwillnet@gmail.com>
2020-07-08 09:47:57 +0900

    ルーティングの未使用アクション定義を削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
 
-  resource :retirements
+  resource :retirements, only: %i[new create]
 
-  resources :events do
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
   get 'status' => 'status#index', defaults: { format: 'json' }
 end
